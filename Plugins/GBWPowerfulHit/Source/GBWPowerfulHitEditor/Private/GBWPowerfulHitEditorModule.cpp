e4f6b964819fad733184bda417471706c62da73a
BluceLiSr1o6x <1085889471@qq.com>
2023-08-30 21:33:43 +0800

    8.30-去除插件，修复攻击和特攻冲突bug

--- Plugins/GBWPowerfulHit/Source/GBWPowerfulHitEditor/Private/GBWPowerfulHitEditorModule.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright GanBowen 2022-2023. All Rights Reserved.
-#include "GBWPowerfulHitEditorModule.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_MODULE(FDefaultModuleImpl, GBWPowerfulHitEditor)
